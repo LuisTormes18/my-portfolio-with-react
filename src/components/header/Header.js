ee9c595c5f1dbf642e9786df41102d2fea291979
Luis Tormes <luisangeltormes@gmail.coom>
2021-11-03 10:44:28 -0400

    updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,45 +1,43 @@
 import React, { useState } from "react";
 
 import Logo from "./../global/Logo";
-import MenuItem from './MenuItem';
-import './header.css';
+import MenuItem from "./MenuItem";
+import "./header.css";
 
 function Header() {
-    const [menuMovil, setMenuMovil] = useState(null);
-    const [sticky, setSticky] = useState('');
+    const [menuMovil, setMenuMovil] = useState(false);
+    const [sticky, setSticky] = useState("");
 
-    function toogleMenu() {
-        if (menuMovil) {
-            setMenuMovil(null);
-        } else {
-            setMenuMovil("active-menu-mobile");
-        }
-    }
+    const classMenuMovil = menuMovil ? "active-menu-mobile" : "";
+
     window.onscroll = () => {
-    let Scrollposition = window.pageYOffset;
+        let scrollPosition = window.pageYOffset;
 
-    if (Scrollposition > 319) {
-        setSticky('sticky');
-    } else {
-        setSticky('');
-    }
+        if (scrollPosition > 319) {
+            setSticky("sticky");
+        } else {
+            setSticky("");
+        }
     };
 
     return (
         <header className={`header ${sticky}`}>
             <Logo />
-           
-            <nav id="navbar" className={menuMovil}>
-                <button onClick={toogleMenu} id="btn-mobile">
+
+            <nav id="navbar" className={classMenuMovil}>
+                <button
+                    onClick={() => {
+                        setMenuMovil(!menuMovil);
+                    }}
+                    id="btn-mobile"
+                >
                     <span id="hamburger"> </span>
                 </button>
                 <ul id="menu">
-
-                <MenuItem text='Home' to='hero' />
-                <MenuItem text='About' to='session-about' />
-                <MenuItem text='Projects' to='session-projects' />
-                <MenuItem text='Skills' to='session-skills' />
-                 
+                    <MenuItem text="Home" to="hero" />
+                    <MenuItem text="About" to="session-about" />
+                    <MenuItem text="Projects" to="session-projects" />
+                    <MenuItem text="Skills" to="session-skills" />
                 </ul>
             </nav>
         </header>
--- src/components/header/MenuItem.js
+++ src/components/header/MenuItem.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { Link } from 'react-scroll';
+import React from "react";
+import { Link } from "react-scroll";
 
-export default function MenuItem({text,to}) {
-	return (
-		<li>
-          <Link className='pointer' to={to}  smooth={true} duration={1100}>{text}</Link>
+export default function MenuItem({ text, to }) {
+    return (
+        <li>
+            <Link className="pointer" to={to} smooth={true} duration={1100}>
+                {text}
+            </Link>
         </li>
-	)
+    );
 }
