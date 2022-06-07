d3da3379861fd7bc8d0d3ecf925d2695cb9ae214
Luis Tormes <luisangeltormes@gmail.com>
2022-06-07 12:56:11 -0400

    cv actualizado, y modifique algunos estilos e imagenes

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 import "animate.css";
-import "./index.css";
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
